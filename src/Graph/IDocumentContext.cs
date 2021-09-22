4c845eeefa52e2c6e5512adb8223df0cf1f85b72
Mark Lauter <Mark.Lauter@insight.com>
2021-09-22 01:35:33 -0400

    commented out graph code until tomorrow

--- src/Graph/IDocumentContext.cs
+++ src/Graph/IDocumentContext.cs
@@ -1,14 +1,14 @@
-﻿using Documents.Collections;
-using Graphs.Elements;
-using System;
+﻿//using Documents.Collections;
+//using Graphs.Elements;
+//using System;
 
-namespace Graphs.Data
-{
-    public interface IDocumentContext<TId>
-        where TId : struct, IComparable, IComparable<TId>, IEquatable<TId>, IFormattable
-    {
-        public IDocumentCollection<Edge<TId>> Edges { get; }
-        public IIdGenerator<TId> IdGenerator { get; }
-        public IDocumentCollection<Node<TId>> Nodes { get; }
-    }
-}
+//namespace Graphs.Data
+//{
+//    public interface IDocumentContext<TId>
+//        where TId : struct, IComparable, IComparable<TId>, IEquatable<TId>, IFormattable
+//    {
+//        public IDocumentCollection<Edge<TId>> Edges { get; }
+//        public IIdGenerator<TId> IdGenerator { get; }
+//        public IDocumentCollection<Node<TId>> Nodes { get; }
+//    }
+//}
