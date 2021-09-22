efcb1ccb22328a486a2a53e9fc6bf540acaae533
Mark Lauter <Mark.Lauter@insight.com>
2021-09-21 20:20:45 -0400

    started graph data context, moved some files

--- src/Graph/DocumentContext{TId}.cs
+++ src/Graph/DocumentContext{TId}.cs
@@ -1,10 +1,31 @@
 ﻿using Documents.Collections;
 using Graphs.Elements;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Graphs.Data
 {
-    public sealed class DocumentContext<TId>
+    public interface IGraphContext<TElement, TId>
+        where TElement : IMutableElement<TId>
+        where TId : struct, IComparable, IComparable<TId>, IEquatable<TId>, IFormattable
+    {
+        Task AddAsync(TElement element);
+        Task AddAsync(IEnumerable<TElement> elements);
+
+        Task<bool> ContainsAsync(TElement element);
+
+        Task<TElement> ReadAsync(TId id);
+        Task<IEnumerable<TElement>> ReadAsync(IEnumerable<string> ids);
+
+        Task RemoveAsync(TElement element);
+        Task RemoveAsync(IEnumerable<TElement> elements);
+
+        Task UpdateAsync(TElement element);
+        Task UpdateAsync(IEnumerable<TElement> elements);
+    }
+
+    public sealed class GraphDataContext<TId>
         : IDocumentContext<TId>
         where TId : struct, IComparable, IComparable<TId>, IEquatable<TId>, IFormattable
     {
@@ -12,7 +33,7 @@ namespace Graphs.Data
         public IIdGenerator<TId> IdGenerator { get; }
         public IDocumentCollection<Node<TId>> Nodes { get; }
 
-        public DocumentContext(
+        public GraphDataContext(
             IIdGenerator<TId> idGenerator,
             IDocumentCollection<Node<TId>> nodes,
             IDocumentCollection<Edge<TId>> edges)
