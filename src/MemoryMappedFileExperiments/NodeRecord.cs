fe699780fa978993c6f4600398ce689c54066917
marklauter <Mark.Lauter@insight.com>
2021-10-09 01:28:54 -0400

    trying out memory mapped files

--- src/MemoryMappedFileExperiments/NodeRecord.cs
+++ src/MemoryMappedFileExperiments/NodeRecord.cs
@@ -3,6 +3,29 @@
 namespace MemoryMappedFileExperiments
 {
     [DebuggerDisplay("{Offset}, {Degree}, Deleted {IsDeleted}, => {FirstEdgeOffset}")]
+    public struct Node
+    {
+        internal Node(
+            long firstEdgeOffset,
+            bool isDeleted,
+            int degree)
+        {
+            this.Degree = degree;
+            this.IsDeleted = isDeleted;
+            this.FirstEdgeOffset = firstEdgeOffset;
+        }
+
+        // offset 0
+        public long FirstEdgeOffset { get; }
+
+        // offset sizeof(long)
+        public bool IsDeleted { get; }
+
+        // offset sizeof(long) + sizeof(bool)
+        public int Degree { get; }
+    }
+
+    [DebuggerDisplay("{Offset}, {Degree}, Deleted {IsDeleted}, => {FirstEdgeOffset}")]
     public struct NodeRecord
     {
         internal NodeRecord(
--- src/MemoryMappedFileExperiments/Program.cs
+++ src/MemoryMappedFileExperiments/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.MemoryMappedFiles;
 using System.Runtime.InteropServices;
 
 namespace MemoryMappedFileExperiments
@@ -9,31 +10,52 @@ namespace MemoryMappedFileExperiments
     {
         private static void Main(string[] args)
         {
-            using var nodeMemoryStream = new MemoryStream();
-            using var edgeMemoryStream = new MemoryStream();
-            using var nodeStream = new NodeStream(nodeMemoryStream);
-            using var edgeStream = new EdgeStream(edgeMemoryStream, nodeStream);
+            using var mmNodeFile = MemoryMappedFile
+                .CreateFromFile("nodes.dat", FileMode.OpenOrCreate, null, 1024 * 1024 * 5);
+            using var mmNodeViewStream = mmNodeFile
+                .CreateViewStream(0x00, 1024 * 1024);
+
+            using var mmEdgeFile = MemoryMappedFile
+                .CreateFromFile("edges.dat", FileMode.OpenOrCreate, null, 1024 * 1024 * 5);
+            using var mmEdgeViewStream = mmEdgeFile
+                .CreateViewStream(0x00, 1024 * 1024);
+
+
+            using var nodeAccessor = mmNodeFile.CreateViewAccessor(0, 1024 * 1024);
+
+            var nodeSize = Marshal.SizeOf(typeof(Node));
+            var node = new Node(1, false, 1);
+            nodeAccessor.Write(0, ref node);
+            node = new Node(2, false, 2);
+            nodeAccessor.Write(nodeSize, ref node);
+
+
+
+            //using var nodeMemoryStream = new MemoryStream();
+            //using var edgeMemoryStream = new MemoryStream();
+            //using var nodeStream = new NodeStream(mmNodeViewStream);
+            //using var edgeStream = new EdgeStream(mmEdgeViewStream, nodeStream);
             
-            var nodes = new List<NodeRecord>(10);
-            for (var i = 0; i < 10; ++i)
-            {
-                nodes.Add(nodeStream.Add());
-            }
+            //var nodes = new List<NodeRecord>(10);
+            //for (var i = 0; i < 10; ++i)
+            //{
+            //    nodes.Add(nodeStream.Add());
+            //}
 
-            edgeStream.Connect(nodes[0].Offset, nodes[1].Offset);
-            edgeStream.Connect(nodes[0].Offset, nodes[2].Offset);
+            //edgeStream.Connect(nodes[0].Offset, nodes[1].Offset);
+            //edgeStream.Connect(nodes[0].Offset, nodes[2].Offset);
 
-            var node1 = nodeStream.Read(nodes[0].Offset);
-            var node2 = nodeStream.Read(nodes[1].Offset);
-            if (node1.FirstEdgeOffset == NodeStream.DefaultFirstEdgeOffset)
-                throw new InvalidOperationException("bad edge");
+            //var node1 = nodeStream.Read(nodes[0].Offset);
+            //var node2 = nodeStream.Read(nodes[1].Offset);
+            //if (node1.FirstEdgeOffset == NodeStream.DefaultFirstEdgeOffset)
+            //    throw new InvalidOperationException("bad edge");
 
-            if (node2.FirstEdgeOffset == NodeStream.DefaultFirstEdgeOffset)
-                throw new InvalidOperationException("bad edge");
+            //if (node2.FirstEdgeOffset == NodeStream.DefaultFirstEdgeOffset)
+            //    throw new InvalidOperationException("bad edge");
 
-            var edge1 = edgeStream.Read(node1.FirstEdgeOffset);
-            var edge2 = edgeStream.Read(node2.FirstEdgeOffset);
-            var edge3 = edgeStream.Read(edge1.NextRecordOffset);
+            //var edge1 = edgeStream.Read(node1.FirstEdgeOffset);
+            //var edge2 = edgeStream.Read(node2.FirstEdgeOffset);
+            //var edge3 = edgeStream.Read(edge1.NextRecordOffset);
 
             // todo: next step would be to run a breadth first traversal down the linked list of edges
         }
