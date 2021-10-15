695782e9446bdb1543af2f73d1bbf03632c69327
marklauter <Mark.Lauter@insight.com>
2021-10-15 16:46:48 -0400

    binary tree

--- src/MemoryMappedFileExperiments/Program.cs
+++ src/MemoryMappedFileExperiments/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Buffers;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Runtime.InteropServices;
@@ -21,8 +22,143 @@ namespace MemoryMappedFileExperiments
 
         private static void Main(string[] args)
         {
+            BinaryInsertLinkedListExperiment();
+        }
+
+        [DebuggerDisplay("{Data} ({Left}, {Right})")]
+        private readonly struct ListItem
+        {
+            public ListItem(long data)
+                : this(-1, -1, data)
+            {
+                this.Data = data;
+            }
+
+            public ListItem(long left, long right, long data)
+            {
+                this.Left = left;
+                this.Right = right;
+                this.Data = data;
+            }
+
+            internal readonly long Left;
+            internal readonly long Right;
+            internal readonly long Data;
+        }
+
+        private const long RootOffset = 0;
+        private const int MaxRng = 100;
+
+        private static void BinaryInsertLinkedListExperiment()
+        {
+            var rng = new Random(DateTime.UtcNow.Millisecond);
+            var list = new ListItem[10];
+
+            for (var i = 0; i < 10; ++i)
+            {
+                list[i] = new ListItem(rng.Next(MaxRng));
+                Insert(i, RootOffset, list);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine();
+
+            Traverse(RootOffset, "root", list);
+        }
+
+        private static void Traverse(long offset, string direction, ListItem[] items)
+        {
+            if (offset < 0)
+            {
+                return;
+            }
+
+            var item = items[offset];
+            Console.WriteLine($"{direction}: {offset} : {item.Data}, ({item.Left}, {item.Right})");
+            Traverse(item.Left, "left", items);
+            Traverse(item.Right, "right", items);
+        }
+
+        private static void Insert(long newOffset, long currentOffset, ListItem[] items)
+        {
+            if (newOffset == currentOffset)
+            {
+                return;
+            }
+
+            var currentItem = items[currentOffset];
+            var newItem = items[newOffset];
+
+            Console.WriteLine($"item data: new ({newOffset}, {newItem.Data}), current:  ({currentOffset}, {currentItem.Data}, {currentItem.Left}, {currentItem.Right})");
+
+            if (newItem.Data < currentItem.Data)
+            {
+                if (currentItem.Left > -1)
+                {
+                    Console.WriteLine("move left");
+                    Insert(newOffset, currentItem.Left, items);
+                }
+                else
+                {
+                    Console.WriteLine("insert left");
+                    items[currentOffset] = new ListItem(newOffset, currentItem.Right, currentItem.Data);
+                }
+            }
+            else
+            {
+                if (currentItem.Right > -1)
+                {
+                    Console.WriteLine("move right");
+                    Insert(newOffset, currentItem.Right, items);
+                }
+                else
+                {
+                    Console.WriteLine("insert right");
+                    items[currentOffset] = new ListItem(currentItem.Left, newOffset, currentItem.Data);
+                }
+            }
+        }
+
+        private static void StreamAndStructExperiment()
+        {
+            using var nodeMemoryStream = new MemoryStream();
+            using var edgeMemoryStream = new MemoryStream();
+            using var nodeStream = new NodeStream(nodeMemoryStream);
+            using var edgeStream = new EdgeStream(edgeMemoryStream, nodeStream);
+
+            var nodes = new List<NodeRecord>(10);
+            for (var i = 0; i < 10; ++i)
+            {
+                nodes.Add(nodeStream.Add());
+            }
+
+            edgeStream.Connect(nodes[0].Offset, nodes[1].Offset);
+            edgeStream.Connect(nodes[0].Offset, nodes[2].Offset);
+
+            var node1 = nodeStream.Read(nodes[0].Offset);
+            var node2 = nodeStream.Read(nodes[1].Offset);
+            if (node1.FirstEdgeOffset == NodeStream.DefaultFirstEdgeOffset)
+            {
+                throw new InvalidOperationException("bad edge");
+            }
+
+            if (node2.FirstEdgeOffset == NodeStream.DefaultFirstEdgeOffset)
+            {
+                throw new InvalidOperationException("bad edge");
+            }
+
+            var edge1 = edgeStream.Read(node1.FirstEdgeOffset);
+            var edge2 = edgeStream.Read(node2.FirstEdgeOffset);
+            var edge3 = edgeStream.Read(edge1.NextRecordOffset);
+
+            //todo:next step would be to run a breadth first traversal down the linked list of edges
+        }
+
+        private static void MMFExperiment()
+        {
             using var mmNodeFile = MemoryMappedFile
-                .CreateFromFile("nodes.dat", FileMode.OpenOrCreate, null, 1024 * 1024 * 5);
+                            .CreateFromFile("nodes.dat", FileMode.OpenOrCreate, null, 1024 * 1024 * 5);
             using var mmNodeViewStream = mmNodeFile
                 .CreateViewStream(0x00, 1024 * 1024);
 
@@ -37,48 +173,17 @@ namespace MemoryMappedFileExperiments
             // https://docs.microsoft.com/en-us/dotnet/standard/memory-and-spans/memory-t-usage-guidelines
             using var nodeAccessor = mmNodeFile.CreateViewAccessor(0, 1024 * 1024);
 
-            var memoryOwner = 
+            var memoryOwner =
                 MemoryPool<MemoryMappedNode>.Shared.Rent();
             var memory = memoryOwner.Memory;
 
             var nodeSize = Marshal.SizeOf(typeof(MemoryMappedNode));
-            
-            for(var offset = 0; offset < 10; ++offset)
+
+            for (var offset = 0; offset < 10; ++offset)
             {
                 var node = new MemoryMappedNode(offset + 1, false, offset + 1);
                 nodeAccessor.Write(offset * nodeSize, ref node);
             }
-
-
-            var nodeSpan = new Span<NodeRecord>();
-
-            //using var nodeMemoryStream = new MemoryStream();
-            //using var edgeMemoryStream = new MemoryStream();
-            //using var nodeStream = new NodeStream(mmNodeViewStream);
-            //using var edgeStream = new EdgeStream(mmEdgeViewStream, nodeStream);
-            
-            //var nodes = new List<NodeRecord>(10);
-            //for (var i = 0; i < 10; ++i)
-            //{
-            //    nodes.Add(nodeStream.Add());
-            //}
-
-            //edgeStream.Connect(nodes[0].Offset, nodes[1].Offset);
-            //edgeStream.Connect(nodes[0].Offset, nodes[2].Offset);
-
-            //var node1 = nodeStream.Read(nodes[0].Offset);
-            //var node2 = nodeStream.Read(nodes[1].Offset);
-            //if (node1.FirstEdgeOffset == NodeStream.DefaultFirstEdgeOffset)
-            //    throw new InvalidOperationException("bad edge");
-
-            //if (node2.FirstEdgeOffset == NodeStream.DefaultFirstEdgeOffset)
-            //    throw new InvalidOperationException("bad edge");
-
-            //var edge1 = edgeStream.Read(node1.FirstEdgeOffset);
-            //var edge2 = edgeStream.Read(node2.FirstEdgeOffset);
-            //var edge3 = edgeStream.Read(edge1.NextRecordOffset);
-
-            // todo: next step would be to run a breadth first traversal down the linked list of edges
         }
     }
 }
